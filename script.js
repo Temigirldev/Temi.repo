14d53cf37a074266b577ee7ac767ba2a5963120d
temidevgirl <esthertemitope17@gmail.com>
2023-02-16 20:16:51 +0100

    completed

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementsByClassName('.toggle-button')[0]
-const navlist = document.getElementsByClassName('.navlist')[0]
-
-toggleButton.addEventListener('click' , () => {
-    navlist.classlist.toggle('.active')
-})
